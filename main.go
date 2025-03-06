f98984019bdf27f9acac673fe829264d25857ddc
Jason <me@jasonuc.com>
2025-03-06 07:39:50 +0000

    feat: integrate Ebiten for game development and set up basic game structure

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+type Game struct{}
+
+func NewGame() *Game {
+	return &Game{}
+}
+
+func (g *Game) Update() error {
+	if inpututil.IsKeyJustPressed(ebiten.KeyQ) {
+		return ebiten.Termination
+	}
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
+
+func main() {
+	ebiten.SetWindowSize(1280, 720)
+	ebiten.SetWindowTitle("minecraft inventory system")
+	if err := ebiten.RunGame(NewGame()); err != nil {
+		log.Fatal(err)
+	}
+}
